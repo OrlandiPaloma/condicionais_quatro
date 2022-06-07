3b84d1ec95526a70c059829fd4d27c3d410352bb
Ubiratan <palomaorlandiw@gmail.com>
2022-06-07 16:08:23 -0300

    condicionais_quatro

---
+++ condicionais_quatro.py
@@ -0,0 +1,16 @@
+# Condicionais
+# if, elif, else
+'''
+input primeiro valor
+input segundo valor
+if primeiro_valor > segundo valor
+  print o primeiro valor é maior
+else
+print o segundo valor é maior
+'''
+primeiro_valor = 2
+segundo_Valor = 8
+if primeiro_valor > segundo_Valor:
+  print("primeiro valor é maior")
+else:
+  print("segundo valor é maior")
